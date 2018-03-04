e371e16fbb2f6d1cbe730abc1c9720d9b3da95cb
jorgelopez95 <jorge.lgi@hotmail.com>
2018-03-04 22:03:06 +0000

    logo and index navbar added

--- index.php
+++ index.php
@@ -1,16 +1,57 @@
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
-<title>EtR Advisor</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Application that provides an easy-to-read analysis">
+    <meta name="author" content="Jorge López-Guerrero Iglesias">
+    <title>Easy-to-Read Advisor</title>
+    <link rel="shortcut icon" type="image/png" href="./images/logo_etr.png"/>  
+    
+    <!-- Bootstrap CDN -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384 BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+   
+    <!-- Custom styles -->
+    <link rel="stylesheet" href="styles.css">
 </head>
     
 <body>
-<?php
 
-echo 'Hello world!';
-
-?>
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-inverse navbar-static-top">
+        <div class="container">
+            <div class="row">
+              <div class="navbar-header col-md-2 col-sm-2">
+                  <img src="/images/logo_etr.png" alt="ETR Advisor"> 
+              </div>
+              <div class="navbar-header col-md-5 col-sm-8">
+                  <h1>Easy-to-Read Advisor
+                      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbarMain">
+                          <span class="sr-only">Toggle navigation</span>
+                          <span class="icon-bar"></span>
+                          <span class="icon-bar"></span>
+                          <span class="icon-bar"></span>
+                      </button>
+                  </h1>
+              </div>
+              <div id="navbarMain" class="navbar-collapse collapse col-md-5 col-sm-2">
+                  <ul class="nav navbar-nav navbar-right">
+                      <li><a href="/"><span class="glyphicon glyphicon-home" style="margin-right: 10px;"></span>Inicio</a></li>
+                      <li><a href="#"><span class="glyphicon glyphicon-eye-open" style="margin-right: 10px;"></span>Información</a></li>
+                      <li><a href="#"><span class="glyphicon glyphicon-book" style="margin-right: 10px;"></span>Lectura Fácil</a></li>
+                      <li><a href="#"><span class="glyphicon glyphicon-envelope" style="margin-right: 10px;"></span>Contacto</a></li>
+                  </ul>
+              </div>
+          </div>
+        </div>
+    </nav>
+    
+    
+    
+    <!-- Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 </body>
 
 </html>
